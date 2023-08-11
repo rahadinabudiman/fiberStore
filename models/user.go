9af65a8591778c823a67e23c733650fa471b86eb
R4HA <erbees.50000@gmail.com>
2023-08-11 19:16:42 +0700

    Fix: Username FK & Update JWT Role
    User who have role Admin can using customer  routes, but customer can't use admin routes

--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name     string `json:"name" form:"name" binding:"required"`
-	Username string `gorm:"unique" json:"username" form:"username" binding:"required"`
+	Username string `json:"username" form:"username" binding:"required"`
 	Password string `json:"password" form:"password" binding:"required"`
 	Role     string `gorm:"type:enum('Customer', 'Admin');default:'Customer'; not-null" example:"Admin" json:"role" form:"role"`
 }
