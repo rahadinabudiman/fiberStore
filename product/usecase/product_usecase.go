62057e45b50e0a646e6e9545d18d562900cc4c88
R4HA <erbees.50000@gmail.com>
2023-08-12 16:59:05 +0700

    feat: Update CRUD Product & Seeding Account

--- product/usecase/product_usecase.go
+++ product/usecase/product_usecase.go
@@ -74,21 +74,188 @@ func (pu *productUsecase) InsertOne(ctx context.Context, req *dtos.InserProductR
 }
 
 func (pu *productUsecase) FindAll(ctx context.Context, page, limit int) (*[]dtos.ProductResponse, int, error) {
-	return nil, 0, nil
+	var productResponses []dtos.ProductResponse
+
+	_, cancel := context.WithTimeout(ctx, pu.contextTimeout)
+	defer cancel()
+
+	products, count, err := pu.ProductRepository.FindAll(page, limit)
+	if err != nil {
+		return nil, 0, errors.New("error getting products")
+	}
+
+	for _, product := range *products {
+		productResponses = append(productResponses, dtos.ProductResponse{
+			ID:       product.ID,
+			Slug:     product.Slug,
+			Name:     product.Name,
+			Detail:   product.Detail,
+			Price:    product.Price,
+			Stock:    product.Stock,
+			Category: product.Category,
+			Image:    product.Image,
+		})
+	}
+
+	return &productResponses, count, nil
 }
 
 func (pu *productUsecase) FindQueryAll(ctx context.Context, page, limit int, search string) (*[]dtos.ProductResponse, int, error) {
-	return nil, 0, nil
+	var productResponses []dtos.ProductResponse
+
+	_, cancel := context.WithTimeout(ctx, pu.contextTimeout)
+	defer cancel()
+
+	products, count, err := pu.ProductRepository.FindQueryAll(page, limit, search)
+	if err != nil {
+		return nil, 0, errors.New("error getting products")
+	}
+
+	for _, product := range *products {
+		productResponse := dtos.ProductResponse{
+			ID:       product.ID,
+			Slug:     product.Slug,
+			Name:     product.Name,
+			Detail:   product.Detail,
+			Price:    product.Price,
+			Stock:    product.Stock,
+			Category: product.Category,
+			Image:    product.Image,
+		}
+		productResponses = append(productResponses, productResponse)
+	}
+
+	return &productResponses, count, nil
+}
+
+func (pu *productUsecase) FindAllByCategory(ctx context.Context, page, limit int, category string) (*[]dtos.ProductResponse, int, error) {
+	var productResponses []dtos.ProductResponse
+
+	_, cancel := context.WithTimeout(ctx, pu.contextTimeout)
+	defer cancel()
+
+	products, count, err := pu.ProductRepository.FindByCategory(category, page, limit)
+	if err != nil {
+		return nil, 0, errors.New("error getting products")
+	}
+
+	for _, product := range *products {
+		productResponse := dtos.ProductResponse{
+			ID:       product.ID,
+			Slug:     product.Slug,
+			Name:     product.Name,
+			Detail:   product.Detail,
+			Price:    product.Price,
+			Stock:    product.Stock,
+			Category: product.Category,
+			Image:    product.Image,
+		}
+		productResponses = append(productResponses, productResponse)
+	}
+
+	return &productResponses, count, nil
 }
 
 func (pu *productUsecase) FindOne(ctx context.Context, id uint) (*dtos.ProductResponse, error) {
-	return nil, nil
+	var productResponse dtos.ProductResponse
+
+	_, cancel := context.WithTimeout(ctx, pu.contextTimeout)
+	defer cancel()
+
+	product, err := pu.ProductRepository.FindOne(int(id))
+	if err != nil {
+		return nil, errors.New("error getting product")
+	}
+
+	productResponse = dtos.ProductResponse{
+		ID:       product.ID,
+		Slug:     product.Slug,
+		Name:     product.Name,
+		Detail:   product.Detail,
+		Price:    product.Price,
+		Stock:    product.Stock,
+		Category: product.Category,
+		Image:    product.Image,
+	}
+
+	return &productResponse, nil
 }
 
-func (pu *productUsecase) UpdateOne(ctx context.Context, req *dtos.ProductRequest, id uint) (*dtos.ProductResponse, error) {
-	return nil, nil
+func (pu *productUsecase) UpdateOne(ctx context.Context, req *dtos.UpdateProductRequest, id, AdministratorID uint) (*dtos.ProductResponse, error) {
+	_, cancel := context.WithTimeout(ctx, pu.contextTimeout)
+	defer cancel()
+
+	product, err := pu.ProductRepository.FindOne(int(id))
+	if err != nil {
+		return nil, errors.New("product not found")
+	}
+
+	// Check AdministratorID
+	if product.AdministratorID != AdministratorID {
+		return nil, errors.New("not allowed to update product")
+	}
+
+	if product.Name != req.Name {
+		slug := helpers.CreateSlug(req.Name)
+
+		checkSlug, err := pu.ProductRepository.FindOneBySlug(slug)
+		if err == nil {
+			if checkSlug.Slug == slug {
+				slug = slug + "-" + helpers.RandomString(5)
+			}
+		}
+
+		req.Slug = slug
+	}
+
+	if req.Name != "" && req.Detail != "" && req.Price != 0 && req.Stock != 0 && req.Category != "" {
+		product.Name = req.Name
+		product.Detail = req.Detail
+		product.Price = req.Price
+		product.Stock = req.Stock
+		product.Category = req.Category
+	}
+
+	if req.Image != "" {
+		product.Image = req.Image
+	}
+
+	updatedProduct, err := pu.ProductRepository.UpdateOne(product)
+	if err != nil {
+		return nil, errors.New("error updating product")
+	}
+
+	res := &dtos.ProductResponse{
+		ID:       updatedProduct.ID,
+		Slug:     updatedProduct.Slug,
+		Name:     updatedProduct.Name,
+		Detail:   updatedProduct.Detail,
+		Price:    updatedProduct.Price,
+		Stock:    updatedProduct.Stock,
+		Category: updatedProduct.Category,
+		Image:    updatedProduct.Image,
+	}
+
+	return res, nil
 }
 
-func (pu *productUsecase) DeleteOne(ctx context.Context, id uint) error {
+func (pu *productUsecase) DeleteOne(ctx context.Context, id, AdministratorID uint) error {
+	_, cancel := context.WithTimeout(ctx, pu.contextTimeout)
+	defer cancel()
+
+	product, err := pu.ProductRepository.FindOne(int(id))
+	if err != nil {
+		return errors.New("product not found")
+	}
+
+	if product.AdministratorID != AdministratorID {
+		return errors.New("not allowed to delete product")
+	}
+
+	err = pu.ProductRepository.DeleteOne(product)
+	if err != nil {
+		return errors.New("error deleting product")
+	}
+
 	return nil
 }
